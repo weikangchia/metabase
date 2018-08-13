2331cfadd3606ddd2567b9d389121b8a30191b78
Ryan Senior <ryan@metabase.com>
2018-08-13 09:56:24 -0500

    Roll back the 0.30.0 PG driver version bump

--- project.clj
+++ project.clj
@@ -88,7 +88,7 @@
                  [org.clojars.pntblnk/clj-ldap "0.0.12"]              ; LDAP client
                  [org.liquibase/liquibase-core "3.6.2"                ; migration management (Java lib)
                   :exclusions [ch.qos.logback/logback-classic]]
-                 [org.postgresql/postgresql "42.2.2"]                 ; Postgres driver
+                 [org.postgresql/postgresql "42.1.4.jre7"]            ; Postgres driver
                  [org.slf4j/slf4j-log4j12 "1.7.25"]                   ; abstraction for logging frameworks -- allows end user to plug in desired logging framework at deployment time
                  [org.tcrawley/dynapath "0.2.5"]                      ; Dynamically add Jars (e.g. Oracle or Vertica) to classpath
                  [org.xerial/sqlite-jdbc "3.21.0.1"]                  ; SQLite driver
